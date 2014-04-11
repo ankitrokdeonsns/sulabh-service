755288ea7955f9e1a4499bd0f567df8adda810a0
Shweta Dongare <shwetadongare28@gmail.com>
2014-04-11 10:56:50 +0530

    000: Shweta, Manali: took params from journey parameter

--- router.js
+++ router.js
@@ -2,11 +2,9 @@ var journey = require('journey');
 var router = new(journey.Router);
 var geoSpatialRepository = require('./geoSpatialRepository.js').geoSpatialRepository;
 
-router.get('/locations').bind(function (req, res) {
-    var request = require('url').parse(req.url);
-    var latLong = request.query.split("&");
-    var latitude = latLong[0].split("=")[1];
-    var longitude = latLong[1].split("=")[1];
+router.get('/locations').bind(function (req, res, params) {
+    var latitude = params.latitude;
+    var longitude = params.longitude;
     var callBack = function (error, data){
         res.send("locations: "+data);
     };
